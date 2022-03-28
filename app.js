eacc980f117e2055cb43da3a8d6f606e8c85ea23
Julija <julyasya2017@gmail.com>
2022-03-28 18:51:34 +0300

    new files

---
+++ app.js
@@ -0,0 +1,119 @@
+
+
+//Exercise Работа с переменными
+//1
+//let num = 3
+//alert(num)
+//console.log (num)
+//2
+let a = 10
+let b = 2
+alert(a + b)
+console.log (a + b)
+alert(a - b)
+console.log (a - b)
+alert(a * b)
+console.log (a * b)
+alert(a / b)
+console.log (a / b)
+ //3
+let c = 15
+let d = 2
+alert (c + d)
+//let result = 15 + 2
+//console.log (result)
+//4
+let e = 10
+let k = 5
+let l = 2
+alert (e + k + l)
+console.log (e + k + l)
+
+//5
+let m = 17
+let n = 10
+let o = m - n
+console.log (o)
+let p = 7
+let result = o + p
+console.log (result)
+
+//Работа со строками
+//let str = 'Привет, Мир!'
+//console.log (str)
+
+str1='Привет, '
+str2='Мир!'
+//alert (str1 + str2)
+console.log (str1 + str2)
+
+//let name = ( ' Юлія')
+//let text = ('Привіт,')
+ //alert(text + name)
+//console.log (text + name)
+
+let age = 38
+let text = (' Мені' + age + 'років')
+//alert (' Мені ' + age + ' років')
+console.log (' Мені' + age + 'років')
+
+//Функция prompt
+let name = prompt( 'Ваше имя?')
+//alert ('Ваше имя: '+name)
+console.log ('Ваше имя: '+ name)
+
+let number = prompt ( 'Какое число?' )
+//alert( Math.pow(17, 2))
+
+//Обращение к символам строки
+let str = 'abcde'
+alert(str[0])
+console.log (str[0])
+alert (str[2])
+console.log (str[2])
+alert (str[4])
+console.log (str[4])
+
+//let num = '12345'
+alert ( 1 * 2 * 3 * 4 * 5)
+console.log ( 1 * 2 * 3 * 4 * 5)
+
+//Практика
+//  Напишите скрипт, который считает количество секунд в часе, в сутках, в месяце.?
+let date = 60
+alert (60 * 60)
+console.log (60 * 60)
+
+alert (60 * 60 * 24)
+console.log (60 * 60 * 24)
+
+alert (60 * 60 * 24 * 31)
+console.log (60 * 60 * 24 * 31)
+
+//Создайте три переменные - час, минута, секунда. С их помощью выведите текущее время в формате 'час:минута:секунда'.
+let time = 20
+let minute = 33
+let second = 14
+alert ('20: 33: 14')
+console.log ('20: 33: 14')
+
+//Работа с присваиванием и декрементами
+var num = 47
+num  +=7
+num  -= 18
+num  *= 10
+num  /= 15
+alert(num)
+
+var num = 10
+num = num ++
+num = num ++
+num = num --
+alert(num)
+
+
+
+
+
+
+
